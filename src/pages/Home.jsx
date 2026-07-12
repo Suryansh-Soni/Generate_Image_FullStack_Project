715ddb06f33fb47736a99e9b1d897e89268b5dea
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-12 17:02:25 +0530

    Home component

---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  heigth: 100%;
+  overflow-y: scroll;
+`;
+const Home = () => {
+  return <Container>home</Container>;
+};
+
+export default Home;
