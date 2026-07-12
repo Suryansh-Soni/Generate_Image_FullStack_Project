e074d947accd478a51677514146311db9cb1ad13
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-12 20:06:24 +0530

    home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,97 @@
 import React from "react";
 import styled from "styled-components";
+import SearchBar from "../components/SearchBar.jsx";
+import ImageCard from "../components/ImageCard.jsx";
+
 const Container = styled.div`
-  heigth: 100%;
+  height: 100%;
   overflow-y: scroll;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
 `;
+
+const Headline = styled.div`
+  font-size: 34px;
+  font-weight: 500;
+  color: ${({ theme }) => theme.text_primary};
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+
+  @media (max-width: 600px) {
+    font-size: 20px;
+  }
+`;
+
+const Span = styled.div`
+  font-size: 30px;
+  font-weight: 800;
+  color: ${({ theme }) => theme.secondary};
+`;
+
+const Wrapper = styled.div`
+  width: 100%;
+  max-width: 1400px;
+  padding: 32px 0;
+  display: flex;
+  justify-content: center;
+`;
+
+const CardWrapper = styled.div`
+  width: 100%;
+  display: grid;
+  gap: 20px;
+
+  @media (min-width: 1200px) {
+    grid-template-columns: repeat(4, 1fr);
+  }
+
+  @media (min-width: 640px) and (max-width: 1199px) {
+    grid-template-columns: repeat(3, 1fr);
+  }
+
+  @media (max-width: 639px) {
+    grid-template-columns: repeat(2, 1fr);
+  }
+`;
+
 const Home = () => {
-  return <Container>home</Container>;
+  const item = {
+    photo:
+      "https://media.istockphoto.com/id/1381637603/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=w64j3fW8C96CfYo3kbi386rs_sHH_6BGe8lAAAFS-y4=",
+    author: "Suryansh",
+    prompt: "Mountain landscape generated with AI",
+  };
+
+  return (
+    <Container>
+      <Headline>Explore Popular Post !</Headline>
+      <Span>Generate With AI</Span>
+      <SearchBar />
+
+      <Wrapper>
+        <CardWrapper>
+          <ImageCard item={item} />
+          <ImageCard item={item} />
+          <ImageCard item={item} />
+          <ImageCard item={item} />
+          <ImageCard item={item} />
+          <ImageCard item={item} />
+          <ImageCard item={item} />
+          <ImageCard item={item} />
+        </CardWrapper>
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default Home;
