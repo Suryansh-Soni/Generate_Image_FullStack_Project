13c127907f2b1c12e168eea61f6a0b85ebc6b453
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-12 20:06:17 +0530

    create post

--- src/pages/CreatePost.jsx
+++ src/pages/CreatePost.jsx
@@ -1,12 +1,57 @@
 import React from "react";
+import { useState } from "react";
 import styled from "styled-components";
+import GenerateImageForm from "../components/GenerateImageForm.jsx";
+import GeneratedImageCard from "../components/GeneratedImageCard.jsx";
 const Container = styled.div`
-  heigth: 100%;
+  height: 100%;
   overflow-y: scroll;
+  background: ${({ theme }) => theme.bg};
+  padding: 30px 30px;
+  padding-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+
+  @media (max-width: 768px) {
+    padding: 6px 10px;
+  }
+`;
+
+const Wrapper = styled.div`
+  flex: 1;
+  width: 100%;
+  heigth: fit-content;
+  max-width: 1200px;
+  gap: 8%;
+  display: flex;
+  justify-content: center;
+  @media (max-width: 768px) {
+    flex-direction: column;
+  }
 `;
 
 const CreatePost = () => {
-  return (<Container>createPost</Container>)
+  const [post, setPost] = useState("");
+  const [generateImageLoading, setGenerateImageLoading] = useState(false);
+  const [createPostLoading, setCreatePostLoading] = useState(false);
+
+  return (
+    <Container>
+      <Wrapper>
+        <GenerateImageForm
+          post={post}
+          setPost={setPost}
+          createPostLoading={createPostLoading}
+          setGenerateImageLoading={setGenerateImageLoading}
+          setCreatePostLoading={setCreatePostLoading}
+          generateImageLoading={generateImageLoading}
+        />
+        <GeneratedImageCard src={post?.photo} loading={generateImageLoading} />
+      </Wrapper>
+    </Container>
+  );
 };
 
 export default CreatePost;
