6e79e7cbbbc455431163e2867a64bb0e16ff9574
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-12 17:02:08 +0530

    createPost component

---
+++ src/pages/CreatePost.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+const Container = styled.div`
+  heigth: 100%;
+  overflow-y: scroll;
+`;
+
+const CreatePost = () => {
+  return (<Container>createPost</Container>)
+};
+
+export default CreatePost;
