e9cc51cafb667dd941a8112469709d03a8e6fceb
Suryansh Soni <suryanshsoni92@gmail.com>
2026-07-12 17:02:33 +0530

    theme

---
+++ src/utils/Theme.js
@@ -0,0 +1,75 @@
+export const darkTheme = {
+  bg: "#15171E",
+  bgLight: "#1C1E27",
+  bgDark: "#15171E",
+
+  primary: "#007AFF",
+  secondary: "#9747FF",
+
+  navbar: "#1C1E27",
+  arrow: "#AFAFB5",
+
+  menu_primary_text: "#F2F3F4",
+  menu_secondary_text: "#B1B2B3",
+
+  text_primary: "#F2F3F4",
+  text_secondary: "#B1B2B3",
+
+  card: "#1C1E27",
+  card_light: "#191924",
+
+  disabled: "#B1B2B3",
+
+  shadow: "#00000060",
+
+  white: "#FFFFFF",
+  black: "#000000",
+
+  green: "#12F775",
+  yellow: "#FFCC00",
+  red: "#EF5350",
+  orange: "#F7AD63",
+
+  border: "#2D3039",
+  hover: "#252832",
+  input: "#1C1E27",
+  placeholder: "#8A8B91",
+};
+
+export const lightTheme = {
+  bg: "#F9F9F9",
+  bgLight: "#FFFFFF",
+  bgDark: "#F1F1F1",
+
+  primary: "#007AFF",
+  secondary: "#9747FF",
+
+  navbar: "#FFFFFF",
+  arrow: "#5F6368",
+
+  menu_primary_text: "#242B3F",
+  menu_secondary_text: "#6B7280",
+
+  text_primary: "#242B3F",
+  text_secondary: "#6B7280",
+
+  card: "#FFFFFF",
+  card_light: "#F5F5F5",
+
+  disabled: "#B1B2B3",
+
+  shadow: "#00000020",
+
+  white: "#FFFFFF",
+  black: "#000000",
+
+  green: "#12A150",
+  yellow: "#E5A900",
+  red: "#EF5350",
+  orange: "#F7AD63",
+
+  border: "#E5E7EB",
+  hover: "#F1F3F5",
+  input: "#FFFFFF",
+  placeholder: "#9CA3AF",
+};
